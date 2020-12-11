5780e79f7eb714767288f08ee91e525cb1b6b37e
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2020-12-11 20:54:26 +0100

    structure folders

--- domashna-3/src/main/java/parkingfinder/parking/ParkingApplication.java
+++ domashna-3/src/main/java/parkingfinder/parking/ParkingApplication.java
@@ -2,8 +2,10 @@ package parkingfinder.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ParkingApplication {
 
     public static void main(String[] args) {
