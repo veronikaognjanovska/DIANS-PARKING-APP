53e5b9ba515a1ad6d3866e332c6bb5f5ebacb738
Anastasija99 <anastasija.petrovska.5@gmail.com>
2020-12-17 00:06:48 +0100

    Fixed User updates

--- domashna-3/src/main/java/parkingfinder/repository/StreetNameRepository.java
+++
@@ -1,9 +0,0 @@
-package parkingfinder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import parkingfinder.model.StreetName;
-
-@Repository
-public interface StreetNameRepository  extends JpaRepository<StreetName, Integer> {
-}
