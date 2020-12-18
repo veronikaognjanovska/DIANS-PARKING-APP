d244672b4618c7f8ab08daa1bad106ee0f0f1a1d
veronikaognjanovska <62549189+veronikaognjanovska@users.noreply.github.com>
2020-12-18 16:26:34 +0100

    routes
    new branch because could not merge the old one (route-finder-branch -dom3) into main branch

---
+++ domashna-3/src/main/java/parkingfinder/model/StreetName.java
@@ -0,0 +1,24 @@
+package parkingfinder.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class StreetName {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer ID;
+
+    private String streetName;
+
+
+}
