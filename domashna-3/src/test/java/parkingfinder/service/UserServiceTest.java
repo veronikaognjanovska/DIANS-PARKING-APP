4889adde3a0d23be3b64c916ffa0e79d17e48222
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2020-12-20 02:00:14 +0100

    Added test method in UserServiceTest for UpdateUser method in UserService.

--- domashna-3/src/test/java/parkingfinder/service/UserServiceTest.java
+++ domashna-3/src/test/java/parkingfinder/service/UserServiceTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,10 +18,12 @@ import parkingfinder.model.User;
 import parkingfinder.model.exception.InvalidArgumentsException;
 import parkingfinder.repository.UserRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -65,7 +70,7 @@ class UserServiceTest {
 
         User user = new User();
         user.setEmail("user@gmail.com");
-
+        //slucaj 1 - negativen
         when(passwordEncoder.encode(any())).thenReturn("proba");
         when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
         when(userRepository.save(any())).thenReturn(new User());
@@ -73,18 +78,78 @@ class UserServiceTest {
         Exception exception = assertThrows(InvalidArgumentsException.class, () -> {
             userService.signUpUser(user);
         });
-        //slucaj 1 - pozitiven
-
-
+        //slucaj 2 - pozitiven
 
         when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
         Boolean b = userService.signUpUser(user);
         assertTrue(b);
 
+    }
 
+    @Test
+    public void shouldUpdateUser() {
+        SecurityContextHolder.setContext(new SecurityContext() {
+            @Override
+            public Authentication getAuthentication() {
+                return new Authentication() {
+                    @Override
+                    public Collection<? extends GrantedAuthority> getAuthorities() {
+                        return null;
+                    }
 
-        //slucaj 2 - negativen
+                    @Override
+                    public Object getCredentials() {
+                        return null;
+                    }
 
+                    @Override
+                    public Object getDetails() {
+                        return null;
+                    }
+
+                    @Override
+                    public Object getPrincipal() {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isAuthenticated() {
+                        return false;
+                    }
+
+                    @Override
+                    public void setAuthenticated(boolean b) throws IllegalArgumentException {
+
+                    }
+
+                    @Override
+                    public String getName() {
+                        return "email";
+                    }
+                };
+            }
+
+            @Override
+            public void setAuthentication(Authentication authentication) {
+
+            }
+        });
+
+        User user = new User();
+        user.setID(1);
+        user.setEmail("email");
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        when(userRepository.findById(anyInt())).thenReturn(Optional.of(user));
+        when(userRepository.save(any())).thenReturn(user);
+        boolean result  = userService.updateUser(user);
+        assertTrue(result);
+
+        User user2 = new User();
+        user2.setID(2);
+        user2.setEmail("email");
+        Exception exception = assertThrows(InvalidArgumentsException.class, () -> {
+            userService.signUpUser(user2);
+        });
 
 
     }
