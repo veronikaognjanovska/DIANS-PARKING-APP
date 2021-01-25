3974d9cd3b5b35f2f32619795a5458f4c0d9a415
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2021-01-26 00:43:24 +0100

    Added default schedule configuration and javadoc.

---
+++ domashna-4/parking-service/src/main/java/com/parkingfinder/parkingservice/config/ScheduleConfiguration.java
@@ -0,0 +1,14 @@
+package com.parkingfinder.parkingservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/*
+* Schedule configuration class
+* Contains defauld configuration setting from SpringBoot
+* */
+@Configuration
+@EnableScheduling
+public class ScheduleConfiguration {
+
+}
