8be8ce037cdac4d3d67ee3d11bb23593c6f71f4d
MilenaTrajanoska <68651081+MilenaTrajanoska@users.noreply.github.com>
2021-01-26 00:43:03 +0100

    Added ParkingSpot model, repository and rest controller. Added javadoc to all of the classes.

---
+++ domashna-4/parking-service/src/main/java/com/parkingfinder/parkingservice/repository/ParkingSpotRepository.java
@@ -0,0 +1,13 @@
+package com.parkingfinder.parkingservice.repository;
+
+import com.parkingfinder.parkingservice.model.ParkingSpot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*
+* Repository for manipulating with parking spots model from database
+* */
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, String> {
+
+}
---
+++ domashna-4/parking-service/src/main/java/com/parkingfinder/parkingservice/rest/ParkingSpotsFilterController.java
@@ -0,0 +1,56 @@
+package com.parkingfinder.parkingservice.rest;
+
+import com.parkingfinder.parkingservice.model.ParkingSpot;
+import com.parkingfinder.parkingservice.service.ParkingSpotsFilterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+* Rest controller for accessing parking spots data
+* */
+@RestController
+@RequestMapping("/parking/filter")
+public class ParkingSpotsFilterController {
+
+    @Autowired
+    ParkingSpotsFilterService parkingSpotsFilterService;
+
+    /*
+    * Method that returns all available parking spots from the database
+    * @return List<ParkingSpot> - list of all the parking spots available
+    * */
+    @GetMapping("/all")
+    public List<ParkingSpot> filterParkingsAll() {
+        return parkingSpotsFilterService.getParkingSpotsAll();
+    }
+
+    /*
+    * Method that returns a filtered result of parking
+    * spots based on a user requested access level
+    * @param accessLevel - string that represents the user requested access level
+    * @return List<ParkingSpots> - a list of all parking spots
+    * matching the requested access level
+    * */
+    @GetMapping("/access")
+    public List<ParkingSpot> filterParkingByAccess(@RequestParam String accessLevel) {
+        return parkingSpotsFilterService.getParkingSpotsByAccess(accessLevel);
+    }
+
+    /*
+     * Method that returns a filtered result of parking
+     * spots based on a user requested parking type
+     * @param parkingType - string that represents the user requested parking type
+     * @return List<ParkingSpots> - a list of all parking spots
+     * matching the requested parking type
+     * */
+    @GetMapping("/type")
+    public List<ParkingSpot> filterParkingByType(@RequestParam String parkingType) {
+        return parkingSpotsFilterService.getParkingSpotsByType(parkingType);
+    }
+
+}
