15bbba9eab9a4b6842b1070e4d5619b4a8bf1638
Anastasija99 <anastasija.petrovska.5@gmail.com>
2021-01-28 21:04:20 +0100

    Made javadoc comments for WebSecurityConfig

--- domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/config/WebSecurityConfig.java
+++ domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/config/WebSecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.context.request.WebRequest;
 
+/**
+ * Web Security Configuration that enables web security throughout the application
+ * @extends WebSecurityConfigurerAdapter class
+ */
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
@@ -18,10 +23,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUsernamePasswordAuthenticationProvider customUsernamePasswordAuthenticationProvider;
     private final PasswordEncoder passwordEncoder;
+
+    /**
+     * Constructor for the WebSecurityClass
+     * @param authenticationProvider - object of the class CustomUsernamePasswordAuthenticationProvider
+     * @param passwordEncoder - object of the class PasswordEncoder
+     */
     public WebSecurityConfig(CustomUsernamePasswordAuthenticationProvider authenticationProvider, PasswordEncoder passwordEncoder) {
         this.customUsernamePasswordAuthenticationProvider = authenticationProvider;
         this.passwordEncoder = passwordEncoder;
     }
+
+    /**
+     * Method that is responsible for configuration of web security
+     * @param http - object of class HttpSecurity
+     * @throws Exception - throws exception if any of the requests are invalid
+     */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,11 +59,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .deleteCookies("JSESSIONID");
     }
 
+    /**
+     * Method that is responsible for configuration and providing authentication
+     * @param auth - object of class AuthenticationManagerBuilder
+     * @throws Exception if any of the requests are invalid
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(customUsernamePasswordAuthenticationProvider);
     }
 
+    /**
+     * Method that returns any error attributes while dealing with web requests
+     * @return null if there are errors in the web requests
+     */
     @Bean
     public ErrorAttributes errorAttributes() {
         return new ErrorAttributes() {
