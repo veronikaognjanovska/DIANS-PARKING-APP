ad0700776996d341992ab35177f73e48b10b5813
Anastasija99 <anastasija.petrovska.5@gmail.com>
2021-01-28 17:34:01 +0100

    Made javadoc comments for InvalidArgumentsException and for the enumeration UserRole

--- domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/enumeration/UserRole.java
+++ domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/enumeration/UserRole.java
@@ -2,10 +2,24 @@ package com.parkingfinder.userservice.enumeration;
 
 import org.springframework.security.core.GrantedAuthority;
 
+/**
+ * User roles that can be used
+ * @author Anastasija Petrovska
+ */
 public enum UserRole implements GrantedAuthority {
+    /**
+     * Admin role
+     */
     ADMIN,
+    /**
+     * User role
+     */
     USER;
 
+    /**
+     * Method that gets the authority
+     * @return name - name with the authority
+     */
     @Override
     public String getAuthority() {
         return name();
--- domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/model/exception/InvalidArgumentsException.java
+++ domashna-4/user-service/user-service/src/main/java/com/parkingfinder/userservice/model/exception/InvalidArgumentsException.java
@@ -1,7 +1,15 @@
 package com.parkingfinder.userservice.model.exception;
 
+/**
+ * Exception class for invalid arguments
+ * Extends RuntimeException
+ * @author Anastasija Petrovska
+ */
 public class InvalidArgumentsException extends RuntimeException {
 
+    /**
+     * Method that represents the constructor of the InvalidArgumentsException
+     */
     public InvalidArgumentsException() {
         super("Invalid arguments exception");
     }
